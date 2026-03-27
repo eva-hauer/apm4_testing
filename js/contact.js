d9b72dbfc7eec01c8a4c2f9bf9dc962979e3e107
Eva Hauer <s2410456009@fhooe.at>
2026-03-27 11:04:39 +0100

    initial check in

---
+++ js/contact.js
@@ -0,0 +1,22 @@
+export default class Contact{
+
+    constructor({id,name,img}){
+        this.id = id;
+        this.name = name;
+        this.img = img;
+        this.messages = [];
+    }
+
+    addMessage(msg) {
+        this.messages.push(msg);
+    }
+
+    getUsername() {
+        return this.name;
+    }
+
+    setUsername(username) {
+        this.name = username;
+    }
+}
+
